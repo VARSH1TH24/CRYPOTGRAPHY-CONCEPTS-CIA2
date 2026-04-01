bcfcd11b84a65321beb85ac019ec3fe83ec4d972
VARSH1TH24 <167805110+VARSH1TH24@users.noreply.github.com>
2026-04-01 22:24:36 +0530

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
-// Simple polynomial rolling hash
 long long computeHash(string text) {
-    const int p = 31;        // base
-    const int m = 1e9 + 9;   // large prime
+    const int p = 31;
+    const int m = 1e9 + 9;
 
     long long hash = 0;
     long long power = 1;
 
     for (char c : text) {
-        hash = (hash + (c - 'a' + 1) * power) % m;
-        power = (power * p) % m;
+        if (isalpha(c)) {
+            hash = (hash + (tolower(c) - 'a' + 1) * power) % m;
+            power = (power * p) % m;
+        }
     }
 
-    return (hash + m) % m;;
+    return (hash + m) % m;
 }
