805b81a7474964ee241dd114b5f5e313fda4cfed
VARSH1TH24 <167805110+VARSH1TH24@users.noreply.github.com>
2026-04-01 22:25:30 +0530

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -4,18 +4,17 @@
 using namespace std;
 
 long long computeHash(string text) {
-    const int p = 31;
-    const int m = 1e9 + 9;
+    const long long p = 31;
+    const long long m = 1000000009;
 
     long long hash = 0;
-    long long power = 1;
 
     for (char c : text) {
         if (isalpha(c)) {
-            hash = (hash + (tolower(c) - 'a' + 1) * power) % m;
-            power = (power * p) % m;
+            long long value = tolower(c) - 'a' + 1;
+            hash = (hash * p + value) % m;   
         }
     }
 
-    return (hash + m) % m;
+    return hash;   
 }
